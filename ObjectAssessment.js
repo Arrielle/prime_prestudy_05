461ad8171493061cb6c02982cbf02995813cb349
Arrielle <arrielle@Arrielles-MBP.hsd1.mn.comcast.net>
2017-01-29 21:35:09 -0600

    first commit

---
+++ ObjectAssessment.js
@@ -0,0 +1,12 @@
+var avengersMovie = {
+	name : "Avengers",
+	runtime : 143 + " mins",
+	releaseYear : 2012,
+	Director : "Joss Whedon",
+};
+
+function runOnPageLoad() {
+	console.log(avengersMovie);
+};
+
+runOnPageLoad();
